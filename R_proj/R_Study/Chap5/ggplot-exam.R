7a24e2e2dcf23ffc83d5a83b52cc6b8dc143038f
imgm <dovinekr@gmail.com>
2019-06-10 17:36:11 +0900

    Adjusted ggplot-exam.R

--- R_proj/R_Study/Chap5/ggplot-exam.R
+++ R_proj/R_Study/Chap5/ggplot-exam.R
@@ -14,14 +14,15 @@ ggplot(midwest %>%
 
 ## 3 "suv" 차종을 대상으로 평균 cty(도시 연비)가 가장 높은 회사 다섯 곳을 막대 그래프로 표현해 보세요. 막대는 연비가 높은 순으로 정렬하세요
 # class='suv', mean(cty)
-ddply(mpg, 'cty', tran)
-
+mpg
 mpg2 <- mpg %>%
   group_by(manufacturer) %>%
   filter(class=='suv') %>%
-  summarise(average = mean(cty, na.rm=TRUE)) %>% slice(1:5)
+  summarise(average = mean(cty, na.rm=TRUE)) %>% 
+  arrange(desc(average)) %>% 
+  head(5)
 
-ggplot(mpg2, aes(x=reorder(manufacturer, average), y=average)) + theme_bw() + geom_bar(stat='identity')
+ggplot(mpg2, aes(x=reorder(manufacturer, -average), y=average)) + theme_bw() + geom_col(stat='identity')
 
 ## 4
 mpg_ <- mpg
@@ -30,11 +31,11 @@ mpg3 <- mpg %>%
   group_by(class) %>%
   summarise(count=n())
 
-ggplot(mpg3, aes(x=class, y=count)) + theme_bw() + geom_bar(stat='identity')
+ggplot(mpg3, aes(x=class, y=count)) + geom_bar(stat='identity')
 
 ## 5
 economics
-ggplot(economics, aes(x=date, y=psavert)) + theme_bw() + geom_line()
+ggplot(economics, aes(x=date, y=psavert)) + geom_line()
 
 ## 6
 
@@ -43,13 +44,16 @@ subc <- mpg %>% filter(class=='subcompact') %>% select(cty)
 suv <- mpg %>% filter(class=='suv') %>% select(cty)
 
 class_cty <- mpg %>% 
-  select(class) %>%
-  select() %>% 
+  select(class, cty) %>%
   filter(class=='compact' || class=='subcompact' || class=='suv')
 
 class_cty
 
 ggplot()
+
+df3class <- mpg %>% 
+  filter(class %in% c('compact', 'subcompact', 'suv'))
+ggplot(df3class, aes(x=class, y=cty), )
 
 boxplot(comp, subc, suv, col=c('blue', 'yellow', 'pink'),
         names=c('compact','subcompact','suv'),
@@ -63,6 +67,8 @@ cuts <- diamonds %>%
   summarise(counts = n())
 
 ggplot(cuts, aes(x=cut, y=counts)) + geom_bar(stat='identity')
+ggplot(diamonds, aes(x=cut, fill=cut)) + geom_col() # 대박적
+
 
 ## 7-2
 prices <- diamonds %>% 
@@ -70,8 +76,10 @@ prices <- diamonds %>%
   summarise(avg_price=mean(price, na.rm=TRUE))
 
 ggplot(prices, aes(x=cut, y=avg_price, group=1)) + geom_line()
+ggplot(prices, aes(x=cut, y=avg_price, fill=cut)) + geom_col()
 
 ## 7-3
-price2 <- 
+
+df_color
 
 
