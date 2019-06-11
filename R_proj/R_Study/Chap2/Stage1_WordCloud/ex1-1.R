1ba927b912c9d3a7865ba7803cc0f166b7a0466f
imgm <dovinekr@gmail.com>
2019-06-11 10:53:08 +0900

    Adjust ex1-1.R

--- R_proj/R_Study/Chap2/Stage1_WordCloud/ex1-1.R
+++ R_proj/R_Study/Chap2/Stage1_WordCloud/ex1-1.R
@@ -1,13 +1,66 @@
 # EX 1-1. 서울시 응답소 페이지 분석.
 
+# set
+
 install.packages('rJava')
 install.packages('KoNLP')
 install.packages('wordcloud')
 
-Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_211')
+#
 
 library(rJava)
 library(KoNLP)
 library(wordcloud)
+library(RColorBrewer)
 
 useSejongDic()
+
+data1 <- readLines('data/seoul_new.txt')
+data1 # Raw data
+extractNoun('서울시 버스정책을 역행하는 행위를 고발합니다.')
+
+ata2 <- sapply(data1, extractNoun, USE.NAMES=F)
+data2 # 명사들만 있는 리스트 형태의 데이터
+
+head(unlist(data2), 30)
+
+ata3 <- unlist(data2)
+data3 #명사 데이터
+
+data3 <- gsub("\\d+","", data3)
+data3 <- gsub("서울시","", data3)
+data3 <- gsub("서울","", data3)
+data3 <- gsub("요청","", data3)
+data3 <- gsub("제안","", data3)
+data3 <- gsub("","", data3)
+data3 <- gsub("-","", data3)
+data3
+
+write(unlist(data3), "data/seoul_2.txt")
+data4 <- read.table('data/seoul_2.txt')
+data4
+nrow(data4)
+wordcount <- table(data4)
+wordcount
+head(sort(wordcount, decreasing=T), 20)
+
+data3 <- gsub("OO","", data3)
+data3 <- gsub("개선","", data3)
+data3 <- gsub("문제","", data3)
+data3 <- gsub("관리","", data3)
+data3 <- gsub("민원","", data3)
+data3 <- gsub("이용","", data3)
+data3 <- gsub("관련","", data3)
+data3 <- gsub("시장","", data3)
+
+write(unlist(data3), "data/seoul_3.txt")
+data4 <- read.table("data/seoul_3.txt")
+wordcount <- table(data4)
+head(sort(wordcount, decreasing=T), 20)
+
+# library(RColorBrewer)
+palete <- brewer.pal(9, "Set3")
+wordcloud(names(wordcount), freq=wordcount, scale=c(5,1), rot.per = 0.25, min.freq = 1, 
+          random.order = F, random.color = T, colors = palete)
+legend(0.3, 1, "서울시 응답소 요청사항 분석", cex=0.8, fill=NA, border=NA, bg='white',
+       text.col='red', text.font=2, box.col='red')
